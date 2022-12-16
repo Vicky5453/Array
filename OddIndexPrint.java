161899d9e57788c9c4c3a0262aa6d87fab30cd29
Vicky5453 <113967961+Vicky5453@users.noreply.github.com>
2022-12-16 20:33:03 +0530

    Add files via upload

---
+++ OddIndexPrint.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class OddIndexPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int[] sslc = {67,74,84,66,88};
+         for(int i=0;i<sslc.length;i++)
+         {
+         	System.out.println(sslc[i]);
+         	i++;
+         }    
+	}
+
+}
